22d06db96c51a526fdc0b39e83ab7c21857e934d
catdogboy <catdogboy@yahoo.com>
2008-02-25 11:35:39 -0600

    *. Patch up SimpleSesisonManager to log failure     to register with RMI-registry, but not fail with     an exception - to simplify debug+test cycles     with glassfish app-server.     The RMI-registry we launch in our .war gets     screwed up on undeploy/redeploy, so need to     restart the whole app-server on redeploy.
    *. Fix bug to property register asset-type inheritance
       info with derby-cache as assets get loaded.
       DbAssetSaver needs to save the type-info
       even if some of the asset info is already in the
       cache, but the cache info might mark the
       asset with a super type due to a previous call
       to getAssetIdsFrom.

--- src/littleware/asset/server/db/DbCacheManager.java
+++ src/littleware/asset/server/db/DbCacheManager.java
@@ -15,77 +15,74 @@ import littleware.db.*;
  * if its query is not in the query-log.
  */
 public interface DbCacheManager {
-	/**
-	 * Create handler to load the cached result of the
-	 *    AssertRetriever.getDbAssetIdsFrom
-	 * call with the given argument.
-	 *
-	 * @return reader or null if the requested query has not yet been cached
-	 */
-	public JdbcDbReader<Map<String,UUID>,String> makeDbAssetIdsFromLoader ( UUID u_from, AssetType n_type );
-	
-    /**
-     * Create handler to save child-id data
-	 */
-	public JdbcDbWriter<Map<String,UUID>> makeDbAssetIdsFromSaver ( UUID u_from, AssetType n_type );
-    
+
     /**
      * Create handler to load the cached result of the
-	 *    AssetSearchManager.getDbAssetIdsTo
-	 * call with the given arguments.
-	 *
-	 * @return handler or null if data not available in cache
-	 */
-	public JdbcDbReader<Set<UUID>,String> makeDbAssetIdsToLoader ( UUID u_to, AssetType n_type );
-	
+     *    AssertRetriever.getDbAssetIdsFrom
+     * call with the given argument.
+     *
+     * @return reader or null if the requested query has not yet been cached
+     */
+    public JdbcDbReader<Map<String, UUID>, String> makeDbAssetIdsFromLoader(UUID u_from, AssetType n_type);
+
+    /**
+     * Create handler to save child-id data
+     */
+    public JdbcDbWriter<Map<String, UUID>> makeDbAssetIdsFromSaver(UUID u_from, AssetType n_type);
+
+    /**
+     * Create handler to load the cached result of the
+     *    AssetSearchManager.getDbAssetIdsTo
+     * call with the given arguments.
+     *
+     * @return handler or null if data not available in cache
+     */
+    public JdbcDbReader<Set<UUID>, String> makeDbAssetIdsToLoader(UUID u_to, AssetType n_type);
+
     /**
      * Create handler to save AssetSearchManager.getDbAssetIdsTo data
-	 */
-	public JdbcDbWriter<Set<UUID>> makeDbAssetIdsToSaver ( UUID u_to, AssetType n_type );
+     */
+    public JdbcDbWriter<Set<UUID>> makeDbAssetIdsToSaver(UUID u_to, AssetType n_type);
 
-    
-	/** 
-	 * Create a handler to load the home asset id mapping 
-	 *
-	 * @return handler, or null if data not yet available in cache
-	 */
-	public JdbcDbReader<Map<String,UUID>,String> makeDbHomeIdsLoader ();
+    /** 
+     * Create a handler to load the home asset id mapping 
+     *
+     * @return handler, or null if data not yet available in cache
+     */
+    public JdbcDbReader<Map<String, UUID>, String> makeDbHomeIdsLoader();
 
-	/**
-	 * Create handler to save home-id data
-	 */
-	public JdbcDbWriter<Map<String,UUID>> makeDbHomeIdsSaver ();
-	
+    /**
+     * Create handler to save home-id data
+     */
+    public JdbcDbWriter<Map<String, UUID>> makeDbHomeIdsSaver();
 
-	
-	/**
-	 * Create a handler to update the query-cache when updated
-	 * asset data becomes available.
-	 * The handler does not perform an update for an Asset if the
-	 * cache already contains data for the Asset with equal or greater Transaction count.
-	 */
-	public JdbcDbWriter<Asset> makeDbAssetSaver ();
-	
-	/**
-	 * Create a handler to remove deleted-asset data from the query database.
-	 * If the DbEraser is passed a 'null' asset UUID, then all the
-	 * data is earsed out of the database, and the query-log is cleared.
-	 */
-	public JdbcDbWriter<UUID> makeDbEraser ();
-	
-	/**
-	 * Create a handler to save asset-by-name query data
-	 */
-	public JdbcDbWriter<Set<Asset>> makeDbAssetsByNameSaver ( String s_name, AssetType n_type, UUID u_home );
-	
-	/**
-	 * Create a handler to save asset-by-name query data
+    /**
+     * Create a handler to update the query-cache when updated
+     * asset data becomes available.
+     * The handler does not perform an update for an Asset if the
+     * cache already contains data for the Asset with equal or greater Transaction count.
+     */
+    public JdbcDbWriter<Asset> makeDbAssetSaver();
+
+    /**
+     * Create a handler to remove deleted-asset data from the query database.
+     * If the DbEraser is passed a 'null' asset UUID, then all the
+     * data is earsed out of the database, and the query-log is cleared.
+     */
+    public JdbcDbWriter<UUID> makeDbEraser();
+
+    /**
+     * Create a handler to save asset-by-name query data
+     */
+    public JdbcDbWriter<Set<Asset>> makeDbAssetsByNameSaver(String s_name, AssetType n_type, UUID u_home);
+
+    /**
+     * Create a handler to save asset-by-name query data
      *
      * @return handler or null if data not yet available in cache
-	 */
-	public JdbcDbReader<Set<UUID>,String> makeDbAssetsByNameLoader ( String s_name, AssetType n_type, 
-																 UUID u_home );
-	
+     */
+    public JdbcDbReader<Set<UUID>, String> makeDbAssetsByNameLoader(String s_name, AssetType n_type,
+            UUID u_home);
 }
 
 // littleware asset management system

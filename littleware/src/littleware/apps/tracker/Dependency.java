6c437b956b3bc174948a90efb910a4780d9a5f39
Reuben Pasquini <catdogboy@yahoo.com>
2009-07-16 14:28:36 -0500

    Dust off apps.tracker.Comment for use in etdFormat request tracker

--- littleware/src/littleware/apps/tracker/Dependency.java
+++ littleware/src/littleware/apps/tracker/Dependency.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2009 Reuben Pasquini All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the
+ * Lesser GNU General Public License (LGPL) Version 2.1.
+ * You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+
 package littleware.apps.tracker;
 
 import littleware.asset.Asset;
@@ -10,7 +20,4 @@ import littleware.asset.Asset;
  */
 public interface Dependency extends Asset {}
 
-
-// littleware asset management system
-// Copyright (C) 2007 Reuben Pasquini http://littleware.frickjack.com
 
--- littleware/src/littleware/apps/tracker/Queue.java
+++ littleware/src/littleware/apps/tracker/Queue.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2009 Reuben Pasquini All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the
+ * Lesser GNU General Public License (LGPL) Version 2.1.
+ * You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+
 package littleware.apps.tracker;
 
 import java.rmi.RemoteException;
@@ -24,8 +34,4 @@ public interface Queue extends Asset {
      */
     public List<UUID> getTask( TaskStatus n_status );    
 }
-
-
-// littleware asset management system
-// Copyright (C) 2007 Reuben Pasquini http://littleware.frickjack.com
 

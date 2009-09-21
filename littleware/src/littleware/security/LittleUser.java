5c7fcea284fb59739fb970ba5574064bc9abb039
Reuben Pasquini <catdogboy@yahoo.com>
2009-09-21 10:35:35 -0500

    Introduce UserTreeBuilder user-tree template interface

--- littleware/src/littleware/security/LittleUser.java
+++ littleware/src/littleware/security/LittleUser.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2007-2009 Reuben Pasquini All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the
+ * Lesser GNU General Public License (LGPL) Version 2.1.
+ * You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+
 package littleware.security;
 
 /**
@@ -10,7 +20,6 @@ public interface LittleUser extends LittlePrincipal {
      * Little principal-status class
      */
     public static enum Status {
-
         ACTIVE,
         INACTIVE
     }
@@ -22,9 +31,7 @@ public interface LittleUser extends LittlePrincipal {
     public void setStatus(Status n_status);
 
     /** Covariant return-type clone */
+    @Override
     public LittleUser clone();
 }
-
-// littleware asset management system
-// Copyright (C) 2007 Reuben Pasquini http://littleware.frickjack.com
 

a89ef4497e899d2a5ab17d5c1728a33620908fad
Reuben Pasquini <catdogboy@yahoo.com>
2009-10-21 13:51:08 -0500

    Interdocue Demo/SimpleCL demo project

---
+++ Demo/SimpleCL/src/test/java/littleware/demo/simpleCL/PackageTestSuite.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2009 Reuben Pasquini All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of the
+ * Lesser GNU General Public License (LGPL) Version 2.1.
+ * You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+
+
+package littleware.demo.simpleCL;
+
+import java.util.logging.Logger;
+
+/**
+ * Test suite for simpleCL package
+ */
+public class PackageTestSuite {
+    private static final Logger log = Logger.getLogger( PackageTestSuite.class.getName() );
+}

97b1827125e6b2d7bdb1525c0b3f0c45f03158db
Reuben Pasquini <catdogboy@yahoo.com>
2009-10-21 13:51:08 -0500

    Interdocue Demo/SimpleCL demo project

---
+++ Demo/SimpleCL/src/main/java/littleware/demo/simpleCL/CLApp.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2007-2009 Reuben Pasquini All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the
+ * Lesser GNU General Public License (LGPL) Version 2.1.
+ * You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+
+package littleware.demo.simpleCL;
+
+import java.util.logging.Logger;
+
+/**
+ * Command-line application launcher
+ */
+public class CLApp {
+    private static final Logger log = Logger.getLogger( CLApp.class.getName() );
+}

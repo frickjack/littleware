d7dbbd90b5efd57751fa1e08456f0b9487af8fe6
Reuben Pasquini <catdogboy@yahoo.com>
2009-10-22 14:31:02 -0500

    Starting to flesh out app ...

--- Demo/SimpleCL/src/main/java/littleware/demo/simpleCL/CLApp.java
+++ Demo/SimpleCL/src/main/java/littleware/demo/simpleCL/CLApp.java
@@ -10,11 +10,55 @@
 
 package littleware.demo.simpleCL;
 
+import com.google.inject.Inject;
+import java.util.concurrent.ExecutorService;
 import java.util.logging.Logger;
+import littleware.asset.AssetSearchManager;
+import littleware.security.auth.LittleBootstrap;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkEvent;
+import org.osgi.framework.FrameworkListener;
 
 /**
  * Command-line application launcher
  */
-public class CLApp {
+public class CLApp implements BundleActivator {
     private static final Logger log = Logger.getLogger( CLApp.class.getName() );
+    private boolean running = false;
+    private final AssetSearchManager search;
+    private final LittleBootstrap bootstrap;
+
+    @Inject
+    public CLApp( AssetSearchManager search,
+            LittleBootstrap bootstrap,
+            ExecutorService executor
+            ) {
+        this.search = search;
+        this.bootstrap = bootstrap;
+    }
+
+    /** Launch worker thread once OSGi has started */
+    @Override
+    public void start(final BundleContext ctx) throws Exception {
+        if (!running) {
+            ctx.addFrameworkListener(new FrameworkListener() {
+
+                @Override
+                public synchronized void frameworkEvent(FrameworkEvent evt) {
+                    if ((evt.getType() == FrameworkEvent.STARTED) && (!running)) {
+                        running = true;
+                        ctx.removeFrameworkListener(this);
+                        // launch onto dispatch thread
+                        //SwingUtilities.invokeLater( LgoCommandLine.this );
+                        //new Thread(LgoCommandLine.this).start();
+                    }
+                }
+            });
+        }
+    }
+
+    public void stop(BundleContext ctx) throws Exception {
+        
+    }
 }

4bbc1c920c4de0903e635e0913d5c83503374a87
Reuben Pasquini <catdogboy@yahoo.com>
2009-12-07 17:14:10 -0600

    Decouple MySQL ddl schema definition from littleware database and remove mysql asset_function.sql from repo, patch ServerActivator to not register RMI with registry if reg-port <= 0

--- littleware/src/littleware/security/auth/server/ServerActivator.java
+++ littleware/src/littleware/security/auth/server/ServerActivator.java
@@ -7,7 +7,6 @@
  * License. You can obtain a copy of the License at
  * http://www.gnu.org/licenses/lgpl-2.1.html.
  */
-
 package littleware.security.auth.server;
 
 import com.google.inject.Inject;
@@ -44,64 +43,51 @@ import org.osgi.framework.BundleContext;
  * the remote SessionManager
  */
 public class ServerActivator implements BundleActivator {
-    private static final Logger  olog = Logger.getLogger( ServerActivator.class.getName() );
 
-    private final SessionManager   omgr_session;
-    private final CacheManager     omgr_cache;
-    private final int              oi_registry_port;
-    
+    private static final Logger log = Logger.getLogger(ServerActivator.class.getName());
+    private final SessionManager sessionMgr;
+    private final CacheManager cacheMgr;
+    private final int registryPort;
+
     @Inject
-    public ServerActivator( SessionManager mgr_session, 
-            @Named( "int.lw.rmi_port" ) int i_registry_port,
+    public ServerActivator(SessionManager mgr_session,
+            @Named("int.lw.rmi_port") int i_registry_port,
             ServiceProviderRegistry reg_service,
-            final CacheManager       mgr_cache,
+            final CacheManager mgr_cache,
             final AssetSearchManager mgr_search,
             final AssetManager mgr_asset,
-            final AccountManager mgr_account
-            //final AclManager     mgr_acl
+            final AccountManager mgr_account //final AclManager     mgr_acl
             //final DbAuthManager      dbauth
-            )
-    {
-        omgr_session = mgr_session;
-        omgr_cache = mgr_cache;
-        oi_registry_port = i_registry_port;
+            ) {
+        sessionMgr = mgr_session;
+        cacheMgr = mgr_cache;
+        registryPort = i_registry_port;
         //SimpleDbLoginModule.start( mgr_account, dbauth, mgr_transaction );
-        reg_service.registerService( ServiceType.ACCOUNT_MANAGER,
-                new AbstractServiceProviderFactory<AccountManagerService> ( ServiceType.ACCOUNT_MANAGER, mgr_search ) {
+        reg_service.registerService(ServiceType.ACCOUNT_MANAGER,
+                new AbstractServiceProviderFactory<AccountManagerService>(ServiceType.ACCOUNT_MANAGER, mgr_search) {
+
                     @Override
                     public AccountManagerService createServiceProvider(SessionHelper m_helper) throws BaseException, AssetException, GeneralSecurityException, RemoteException {
-                        return new SimpleAccountManagerService( new RmiAccountManager( this.checkAccessMakeProxy(m_helper, false, mgr_account, AccountManager.class ) ));
+                        return new SimpleAccountManagerService(new RmiAccountManager(this.checkAccessMakeProxy(m_helper, false, mgr_account, AccountManager.class)));
                     }
-                }
-        );
-        reg_service.registerService( ServiceType.ASSET_MANAGER,
-                new AbstractServiceProviderFactory<AssetManagerService> ( ServiceType.ASSET_MANAGER, mgr_search ) {
+                });
+        reg_service.registerService(ServiceType.ASSET_MANAGER,
+                new AbstractServiceProviderFactory<AssetManagerService>(ServiceType.ASSET_MANAGER, mgr_search) {
+
                     @Override
                     public AssetManagerService createServiceProvider(SessionHelper m_helper) throws BaseException, AssetException, GeneralSecurityException, RemoteException {
-                        return new SimpleAssetManagerService( new RmiAssetManager( this.checkAccessMakeProxy(m_helper, false, mgr_asset, AssetManager.class )) );
+                        return new SimpleAssetManagerService(new RmiAssetManager(this.checkAccessMakeProxy(m_helper, false, mgr_asset, AssetManager.class)));
                     }
-                }
-        );
-        reg_service.registerService( ServiceType.ASSET_SEARCH,
-                new AbstractServiceProviderFactory<AssetSearchService> ( ServiceType.ASSET_SEARCH, mgr_search ) {
+                });
+        reg_service.registerService(ServiceType.ASSET_SEARCH,
+                new AbstractServiceProviderFactory<AssetSearchService>(ServiceType.ASSET_SEARCH, mgr_search) {
+
                     @Override
                     public AssetSearchService createServiceProvider(SessionHelper m_helper) throws BaseException, AssetException, GeneralSecurityException, RemoteException {
-                        return new SimpleAssetSearchService( new RmiSearchManager( this.checkAccessMakeProxy(m_helper, true, mgr_search, AssetSearchManager.class )) );
+                        return new SimpleAssetSearchService(new RmiSearchManager(this.checkAccessMakeProxy(m_helper, true, mgr_search, AssetSearchManager.class)));
                     }
-                }
-        );
+                });
 
-        /*... No need to register service provider for SESSION_HELPER ... RmiSessionHelper
-         * takes care of it ...
-         *
-        reg_service.registerService( ServiceType.SESSION_HELPER,
-                new ServiceProviderFactory<SessionHelperService> () {
-                    public SessionHelperService createServiceProvider(SessionHelper m_helper) throws BaseException, AssetException, GeneralSecurityException, RemoteException {
-                        return new SessionHelperProxy( m_helper, mgrSessionProxy );
-                    }
-                }
-        );
-         */
     }
 
     /**
@@ -114,46 +100,46 @@ public class ServerActivator implements BundleActivator {
     public void start(BundleContext ctx) throws Exception {
         try {
             Registry rmi_registry = null;
-            final int i_port = oi_registry_port;
+            final int i_port = registryPort;
+            if (i_port > 0) {
+                try {
+                    log.log(Level.INFO, "Looking for RMI registry on port: " + i_port);
+                    rmi_registry = LocateRegistry.createRegistry(i_port);
+                } catch (Exception e) {
+                    log.log(Level.SEVERE, "Failed to locate or start RMI registry on port " + i_port +
+                            " running without exporting root SessionManager object to RMI universe", e);
 
-            try {
-                olog.log(Level.INFO, "Looking for RMI registry on port: " + i_port);
-                rmi_registry = LocateRegistry.createRegistry(i_port);
-            } catch ( Exception e ) {
-               olog.log( Level.SEVERE, "Failed to locate or start RMI registry on port " + i_port +
-                            " running without exporting root SessionManager object to RMI universe", e
-                            );
+                    rmi_registry = LocateRegistry.getRegistry(i_port);
+                }
 
-                rmi_registry = LocateRegistry.getRegistry( i_port );
+                /**
+                 * Need to wrap session manager with an invocation handler,
+                 * because the RMI server thread inherits the ActivationContext
+                 * of the client thread.  Frick.
+                 */
+                /**
+                 * Publish the reference in the Naming Service using JNDI API
+                 * Context jndi_context = new InitialContext();
+                 * jndi_context.rebind("/littleware/SessionManager", om_session );
+                 */
+                rmi_registry.rebind("littleware/SessionManager", sessionMgr);
+            } else {
+                log.log(Level.INFO, "Not exporing RMI registry - port set to: " + i_port);
             }
-
-            /**
-             * Need to wrap session manager with an invocation handler,
-             * because the RMI server thread inherits the ActivationContext
-             * of the client thread.  Frick.
-             */
-            /**
-             * Publish the reference in the Naming Service using JNDI API
-             * Context jndi_context = new InitialContext();
-             * jndi_context.rebind("/littleware/SessionManager", om_session );
-             */
-            rmi_registry.rebind("littleware/SessionManager", omgr_session );
         } catch (Exception e) {
             //throw new AssertionFailedException("Failed to setup SessionManager, caught: " + e, e);
-            olog.log( Level.SEVERE, "Failed to bind to RMI registry " +
-                                " running without exporting root SessionManager object to RMI universe",
-                                e
-                                );
+            log.log(Level.SEVERE, "Failed to bind to RMI registry " +
+                    " running without exporting root SessionManager object to RMI universe",
+                    e);
 
         }
-        olog.log( Level.INFO, "littleware RMI start ok" );
+        log.log(Level.INFO, "littleware RMI start ok");
         // clear the cache at startup for now
-        omgr_cache.clear();
+        cacheMgr.clear();
     }
 
     @Override
     public void stop(BundleContext ctx) throws Exception {
-        olog.log( Level.INFO, "littleware shutdown ok" );
+        log.log(Level.INFO, "littleware shutdown ok");
     }
-
 }

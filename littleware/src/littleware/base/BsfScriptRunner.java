1463ce9174ba8e1696fdab5e519326579f5edc15
Reuben Pasquini <catdogboy@yahoo.com>
2009-12-10 18:13:15 -0600

    More build patches.  Things weren't building for some reason on my laptop after last patch.

--- littleware/src/littleware/base/BsfScriptRunner.java
+++ littleware/src/littleware/base/BsfScriptRunner.java
@@ -1,8 +1,16 @@
+/*
+ * Copyright 2007-2009 Reuben Pasquini All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the
+ * Lesser GNU General Public License (LGPL) Version 2.1.
+ * You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+
 package littleware.base;
 
-import java.io.Writer;
 import java.util.logging.Logger;
-import java.util.logging.Level;
 import java.io.*;
 import java.util.*;
 
@@ -142,7 +150,4 @@ public class BsfScriptRunner implements ScriptRunner {
         return ofactory_cache;
     }
 }
-
-// littleware asset management system
-// Copyright (C) 2007 Reuben Pasquini http://littleware.frickjack.com
 

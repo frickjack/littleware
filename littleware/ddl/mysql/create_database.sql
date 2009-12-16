ab66daefc3e5a263a38773f20992d3a013317d9b
Reuben Pasquini <catdogboy@yahoo.com>
2009-12-15 23:35:10 -0600

    Bump up to netbeans 6.8 build config, remove final from SimpleAssetBuilder state methods

--- littleware/ddl/mysql/create_database.sql
+++ littleware/ddl/mysql/create_database.sql
@@ -1,12 +1,12 @@
 CREATE USER 'littleware'@'localhost' IDENTIFIED BY 'littleware_password';
 
-CREATE USER 'littleware_user' IDENTIFIED BY  'littleware_user_password';
+CREATE USER 'littleware_user'@'localhost' IDENTIFIED BY  'littleware_user_password';
 
 CREATE USER 'fm_rw'@'%' IDENTIFIED BY  'fm_rw';
 
 CREATE DATABASE littleware;
 
-GRANT ALL PRIVILEGES ON littleware.* TO littleware WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON littleware.* TO 'littleware'@'localhost' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON littleware.* TO 'littleware'@'localhost' WITH GRANT OPTION;
 GRANT SUPER ON *.* TO 'littleware'@'localhost';
 

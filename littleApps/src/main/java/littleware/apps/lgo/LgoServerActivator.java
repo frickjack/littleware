3bde1d7cf60a5f88aeb88da8eb4e8525c66b4588
Reuben Pasquini <catdogboy@yahoo.com>
2010-02-02 19:05:47 -0600

    Add 1am auto-shutdown to lgo dispatch server

--- littleApps/src/main/java/littleware/apps/lgo/LgoServerActivator.java
+++ littleApps/src/main/java/littleware/apps/lgo/LgoServerActivator.java
@@ -10,27 +10,53 @@
 package littleware.apps.lgo;
 
 import com.google.inject.Inject;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import littleware.base.Maybe;
+import littleware.security.auth.LittleBootstrap;
+import org.joda.time.DateTime;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-/**
- *
- * @author pasquini
- */
 public class LgoServerActivator implements BundleActivator {
 
+    private static final Logger log = Logger.getLogger(LgoServerActivator.class.getName());
     private final LgoServer.ServerBuilder serverBuilder;
     private Maybe<LgoServer> maybeServer = Maybe.empty();
+    private Maybe<? extends ScheduledFuture<?>> maybeFuture = Maybe.empty();
+    private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+    private final LittleBootstrap bootstrap;
 
     @Inject
-    public LgoServerActivator(LgoServer.ServerBuilder serverBuilder) {
+    public LgoServerActivator(LgoServer.ServerBuilder serverBuilder,
+            LittleBootstrap bootstrap) {
         this.serverBuilder = serverBuilder;
+        this.bootstrap = bootstrap;
     }
 
     @Override
     public void start(BundleContext bc) throws Exception {
         maybeServer = Maybe.something(serverBuilder.launch());
+        final DateTime now = new DateTime();
+        final DateTime tomorrow = now.plusDays(1).minusHours(now.getHourOfDay() - 1);
+        log.log( Level.INFO, "Scheduling auto-shutdown for " + tomorrow );
+        maybeFuture = Maybe.something(executor.schedule(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    log.log(Level.INFO, "Running automatic shutdown");
+                    maybeFuture = Maybe.empty();
+                    bootstrap.shutdown();
+                } finally {
+                    System.exit(0);
+                }
+            }
+        },
+                tomorrow.getMillis() - now.getMillis(), TimeUnit.MILLISECONDS));
     }
 
     @Override
@@ -39,8 +65,15 @@ public class LgoServerActivator implements BundleActivator {
             try {
                 maybeServer.get().shutdown();
             } finally {
-                maybeServer = maybeServer.empty();
+                maybeServer = Maybe.empty();
             }
+        }
+        if (maybeFuture.isSet()) {
+            maybeFuture.get().cancel(false);
+            maybeFuture = Maybe.empty();
+        }
+        if (!executor.isShutdown()) {
+            executor.shutdown();
         }
     }
 }

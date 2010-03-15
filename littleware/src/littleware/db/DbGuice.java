51191f52bba5a823dedfccd6a2db59f045a5474e
Reuben Pasquini <catdogboy@yahoo.com>
2010-03-15 17:42:52 -0500

    Implement DataSourceHandler and setup DbGuice to wrap DataSource bindings with dynamic proxy - regressions pass

--- littleware/src/littleware/db/DbGuice.java
+++ littleware/src/littleware/db/DbGuice.java
@@ -13,6 +13,7 @@ import com.google.inject.Binder;
 
 import com.google.inject.name.Names;
 import java.io.IOException;
+import java.lang.reflect.Proxy;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -51,72 +52,20 @@ public class DbGuice extends PropertiesGuice {
     /**
      * Simple DataSource binder for now
      */
-    public void bindDataSource(Binder binder, String s_name,
-            String s_url) throws SQLException, NamingException {
+    public void bindDataSource(Binder binder, String name,
+            String url) {
         // just hard code to embedded derby provider for now
-        log.log(Level.INFO, "Binding DataSource " + s_name + " to " + s_url);
-        if (s_url.startsWith("jdbc:derby:")) {
-            // figure out if its an embedded or network datasource
-            final Pattern pattern = Pattern.compile("jdbc:derby://(\\w+):(\\d+)/(.+)$");
-            final Matcher match = pattern.matcher(s_url);
-            if (match.find()) {
-                /*...
-                org.apache.derby.jdbc.ClientDataSource40 dataSource = new org.apache.derby.jdbc.ClientDataSource40 ();
-                dataSource.setServerName ( match.group(1) );
-                dataSource.setPortNumber( Integer.parseInt( match.group(2) ) );
-                dataSource.setDatabaseName ( match.group(3) );
-                binder.bind(DataSource.class).annotatedWith(Names.named(s_name)).toInstance(dataSource);
-                 * */
-                org.logicalcobwebs.proxool.ProxoolDataSource data = new org.logicalcobwebs.proxool.ProxoolDataSource(s_name);
-                data.setDriver(org.apache.derby.jdbc.ClientDriver.class.getName());
-                data.setDriverUrl(s_url);
-                data.setUser("ignore");
-                data.setPassword("ignore");
-                data.setMinimumConnectionCount(10);
-                data.setMaximumConnectionCount(30);
-                data.setMaximumActiveTime(60000);
-                binder.bind(DataSource.class).annotatedWith(Names.named(s_name)).toInstance(data);
+        log.log(Level.INFO, "Binding DataSource " + name + " to " + url);
 
-            } else {
-                org.apache.derby.jdbc.EmbeddedDataSource40 data = new org.apache.derby.jdbc.EmbeddedDataSource40();
-                data.setDatabaseName(s_url.substring("jdbc:derby:".length()));
-                binder.bind(DataSource.class).annotatedWith(Names.named(s_name)).toInstance(data);
-            }
-        } else if (s_url.startsWith("jdbc:oracle:")) {
-            final OracleDataSource data = new OracleDataSource();
-            data.setURL(s_url);
-            binder.bind(DataSource.class).annotatedWith(Names.named(s_name)).toInstance(data);
-        } else if (s_url.startsWith("jdbc:postgresql:")) {
-            org.logicalcobwebs.proxool.ProxoolDataSource data = new org.logicalcobwebs.proxool.ProxoolDataSource(s_name);
-            data.setDriver(org.postgresql.Driver.class.getName());
-            data.setDriverUrl(s_url);
+        final DataSourceHandler handler = (new GeneralDSHBuilder()).url(url).name(name).build();
+        binder.bind(DataSource.class).annotatedWith(Names.named(name)).toInstance(
+                (DataSource) Proxy.newProxyInstance(
+                                            DataSource.class.getClassLoader(),
+                                          new Class[] { DataSource.class },
+                                          handler)
+                );
+        binder.bind(DataSourceHandler.class).annotatedWith(Names.named(name)).toInstance(handler);
 
-            String s_user = "littleware_user";
-            String s_password = "secret";
-
-            // set the user/password if possible
-            for (String s_param : s_url.split("\\?\\&")) {
-                if (s_param.startsWith("user=")) {
-                    s_user = s_param.substring("user=".length());
-                } else if (s_param.startsWith("password=")) {
-                    s_password = s_param.substring("password=".length());
-                }
-            }
-            data.setUser(s_user);
-            data.setPassword(s_password);
-            data.setMaximumConnectionCount(10);
-            data.setMaximumActiveTime(60000);
-            binder.bind(DataSource.class).annotatedWith(Names.named(s_name)).toInstance(data);
-        } else if (s_url.startsWith("jdbc:mysql:")) {
-            com.mysql.jdbc.jdbc2.optional.MysqlDataSource data = new com.mysql.jdbc.jdbc2.optional.MysqlDataSource();
-            data.setURL(s_url);
-            binder.bind(DataSource.class).annotatedWith(Names.named(s_name)).toInstance(data);
-        } else if (s_url.startsWith("jndi:")) {
-            DataSource data = (DataSource) new InitialContext().lookup(s_url.substring("jndi:".length()));
-            binder.bind(DataSource.class).annotatedWith(Names.named(s_name)).toInstance(data);
-        } else {
-            log.log(Level.INFO, "Not autobinding datasource of unknown type: " + s_name + " - " + s_url);
-        }
     }
 
     @Override

e80990d1daa9b2b00078c2e0e12262f94070b284
Reuben Pasquini <catdogboy@yahoo.com>
2010-07-07 23:34:34 -0500

    just a little more tracker implementation

--- littleApps/src/main/java/littleware/apps/tracker/SimpleZipUtil.java
+++ littleApps/src/main/java/littleware/apps/tracker/SimpleZipUtil.java
@@ -10,18 +10,79 @@
 
 package littleware.apps.tracker;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.Adler32;
+import java.util.zip.CheckedOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import littleware.base.feedback.Feedback;
 
 /**
  * Check out http://java.sun.com/developer/technicalArticles/Programming/compression/
  */
 public class SimpleZipUtil implements ZipUtil {
+    private static class SimpleInfo implements ZipInfo {
+        private final MemberIndex index;
+        private final File zipFile;
+        public SimpleInfo( MemberIndex index, File zipFile ) {
+            this.index = index;
+            this.zipFile = zipFile;
+        }
+        @Override
+        public MemberIndex getIndex() {
+            return index;
+        }
+
+        @Override
+        public File getZipFile() {
+            return zipFile;
+        }
+    }
 
     @Override
     public ZipInfo zip(File source, Feedback fb) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+      try {
+         BufferedInputStream origin = null;
+         FileOutputStream dest = new
+           FileOutputStream("c:\\zip\\myfigs.zip");
+         CheckedOutputStream checksum = new
+           CheckedOutputStream(dest, new Adler32());
+         ZipOutputStream out = new
+           ZipOutputStream(new
+             BufferedOutputStream(checksum));
+         //out.setMethod(ZipOutputStream.DEFLATED);
+         final int BUFFER = 102400;
+         byte data[] = new byte[BUFFER];
+         // get a list of files from current directory
+         File f = new File(".");
+         String files[] = f.list();
+
+         for (int i=0; i<files.length; i++) {
+            System.out.println("Adding: "+files[i]);
+            FileInputStream fi = new
+              FileInputStream(files[i]);
+            origin = new
+              BufferedInputStream(fi, BUFFER);
+            ZipEntry entry = new ZipEntry(files[i]);
+            out.putNextEntry(entry);
+            int count;
+            while((count = origin.read(data, 0,
+              BUFFER)) != -1) {
+               out.write(data, 0, count);
+            }
+            origin.close();
+         }
+         out.close();
+         System.out.println("checksum:"+checksum.getChecksum().getValue());
+      } catch(Exception e) {
+         e.printStackTrace();
+      }
+      return null;
     }
 
     @Override

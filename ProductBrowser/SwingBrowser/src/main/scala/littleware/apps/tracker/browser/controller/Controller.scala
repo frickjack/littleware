267a90bdc5f8a0a0a1da3a0ab57fcb7f7e016a2e
Reuben Pasquini <catdogboy@yahoo.com>
2010-08-08 22:04:59 -0500

    ProductBrowser add Controller trait

---
+++ ProductBrowser/SwingBrowser/src/main/scala/littleware/apps/tracker/browser/controller/Controller.scala
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2010 Reuben Pasquini All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of the
+ * Lesser GNU General Public License (LGPL) Version 2.1.
+ * You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+
+package littleware.apps.tracker.browser.controller
+
+import littleware.apps.tracker.Product
+
+trait Controller {
+  def createProduct():Product
+}

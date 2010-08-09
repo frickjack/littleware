993b44a4e3d5d4370c36f51d5a4e136efa8e18fc
Reuben Pasquini <catdogboy@yahoo.com>
2010-08-08 22:04:07 -0500

    ProductBrowser begin CliApp setup

--- ProductBrowser/SwingBrowser/src/main/scala/littleware/apps/tracker/browser/CliApp.scala
+++ ProductBrowser/SwingBrowser/src/main/scala/littleware/apps/tracker/browser/CliApp.scala
@@ -10,9 +10,47 @@
 
 package littleware.apps.tracker.browser
 
-object CliApp {
+import java.net.URL
+import java.util.Arrays
+import java.util.logging.Level
+import java.util.logging.Logger
+import javax.security.auth.login.LoginException
+import littleware.apps.lgo.LgoCommandLine
+import littleware.bootstrap.client.ClientBootstrap
+import littleware.security.auth.client.ClientLoginModule
 
-    def main( args:Array[String] ):Unit = {
-      
+object CliApp {
+  val log = Logger.getLogger( getClass.getName )
+  def main( args:Array[String] ):Unit = {
+    val loginBuilder = ClientLoginModule.newBuilder
+    val cleanArgs = if ((args.length > 1) && args(0).matches("^-+[uU][rR][lL]")) {
+      // Currently only support -url argument
+      val sUrl = args(1);
+      val url = new URL(sUrl);
+      loginBuilder.host(url.getHost());
+      if (args.length > 2) {
+        Arrays.copyOfRange(args, 2, args.length);
+      } else {
+        new Array[String](0);
+      }
+    } else {
+      args
     }
+    try {
+      val boot = ClientBootstrap.clientProvider.get.build.automatic(
+        loginBuilder.build
+      )
+      val cl = boot.bootstrap(classOf[LgoCommandLine])
+      //val cl = boot.;
+      val exitCode = cl.run(cleanArgs);
+      boot.shutdown();
+      System.exit( exitCode )
+    } catch {
+      case ex:LoginException => {
+          log.log(Level.SEVERE, "Failed login", ex)
+          System.exit(1)
+        }
+    }
+      
+  }
 }

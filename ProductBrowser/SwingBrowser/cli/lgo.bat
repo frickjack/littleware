982162c8d359221248b9216fddb319bbfa4883b3
Reuben Pasquini <catdogboy@yahoo.com>
2010-09-04 21:01:59 -0500

    SwingBrowser modify cli/ launch scripts to run littleware.lgo.LgoCommandLine and reference logging.properties file

--- ProductBrowser/SwingBrowser/cli/lgo.bat
+++ ProductBrowser/SwingBrowser/cli/lgo.bat
@@ -4,5 +4,5 @@
 if not defined JAVACMD set JAVACMD=java.exe
 if defined JAVA_HOME set JAVACMD="%JAVA_HOME%\bin\java.exe"
 
-%JAVACMD% -cp %~dps0\SwingBrowser.jar littleware.apps.tracker.browser.CliApp %*
+%JAVACMD% -Djava.util.logging.config.file="%~dps0/logging.properties" -cp "%~dps0\SwingBrowser.jar" littleware.lgo.LgoCommandLine %*
 
--- ProductBrowser/SwingBrowser/cli/lgo.sh
+++ ProductBrowser/SwingBrowser/cli/lgo.sh
@@ -3,9 +3,8 @@
 # example: ./v2v.sh -solr /var/solr/vufind-1.0beta/solr -age 0 -min 3096600 -max 3706204 -age 0
 #
 
-cd `dirname $0`
-date
-command="java -Xms1024m -Xmx1024m -cp SwingBrowser.jar littleware.apps.tracker.browser.CliApp $@"
+launchdir=`/bin/dirname $0`
+command="java -Xms1024m -Xmx1024m '-Djava.util.logging.config.file=${launchdir}/logging.properties' -cp '${launchdir}/SwingBrowser.jar' littleware.lgo.LgoCommandLine $@"
 echo "Running command: " $command
 eval "$command" 2>&1
 

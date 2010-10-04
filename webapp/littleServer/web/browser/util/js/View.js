0456452079358a74e36bf45c42713888bb01e62b
Reuben Pasquini <catdogboy@yahoo.com>
2010-10-03 21:55:14 -0500

    littleServer productBrowser javascript adapted to YUI module pattern

---
+++ webapp/littleServer/web/browser/util/js/View.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2010, catdogboy@yahoo.com
+ * General use subject to GPLv2: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+YUI.namespace( "littleware.browser" )
+littleware = YUI.littleware
+
+
+/**
+ * View module, see http://yuiblog.com/blog/2007/06/12/module-pattern/
+ *
+ * @module View
+ * @namespace littleware.browser
+ */
+littleware.browser.View = function() {
+}();
+

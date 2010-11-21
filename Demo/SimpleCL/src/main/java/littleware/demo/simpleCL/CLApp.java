94b5b891e03da512c2397fd4256470355adb80ce
Reuben Pasquini <catdogboy@yahoo.com>
2010-11-21 15:14:47 -0600

    Tweaks to ivy build-template, move littleware/properties to /properties, master-build builds everything including javadoc/scaladoc, and fix Demo/SimpleCL to work with latest setup

--- Demo/SimpleCL/src/main/java/littleware/demo/simpleCL/CLApp.java
+++ Demo/SimpleCL/src/main/java/littleware/demo/simpleCL/CLApp.java
@@ -7,29 +7,36 @@
  * License. You can obtain a copy of the License at
  * http://www.gnu.org/licenses/lgpl-2.1.html.
  */
-
 package littleware.demo.simpleCL;
 
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import littleware.apps.client.ClientBootstrap;
+import javax.security.auth.login.LoginException;
+import littleware.bootstrap.client.ClientBootstrap;
 
 /**
  * Command-line application launcher
  */
 public class CLApp {
-    private static final Logger log = Logger.getLogger( CLApp.class.getName() );
 
-    public static void main( String[] argv ) {
-        final ClientBootstrap bootstrap = new ClientBootstrap( "localhost" );
-        final AppBuilder builder = bootstrap.bootstrap( SimpleCLBuilder.class );
+    private static final Logger log = Logger.getLogger(CLApp.class.getName());
+
+    public static void main(String[] argv) {
         try {
-            System.out.println( builder.argv(Arrays.asList(argv)).build().call() );
-        } catch (Exception ex) {
-            log.log(Level.SEVERE, "Command failed", ex);
-        } finally {
-            bootstrap.shutdown();
+            // automatic authentication uses default remote server specified in
+            // ~/.littleware/littleware.properties or localhost
+            final ClientBootstrap bootstrap = ClientBootstrap.clientProvider.get().build().automatic();
+            final AppBuilder builder = bootstrap.bootstrap(SimpleCLBuilder.class);
+            try {
+                System.out.println(builder.argv(Arrays.asList(argv)).build().call());
+            } catch (Exception ex) {
+                log.log(Level.SEVERE, "Command failed", ex);
+            } finally {
+                bootstrap.shutdown();
+            }
+        } catch (LoginException ex) {
+            log.log(Level.SEVERE, "Failed login", ex);
         }
     }
 }

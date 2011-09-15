4b2de5a59d4be8fc9c201b35bdd3993f5f5a08c0
Reuben Pasquini <catdogboy@yahoo.com>
2011-09-14 20:17:31 -0500

    littleware pull proxool dependency from non-spring (goofy jars) repository

--- littleware/src/littleware/bootstrap/AppModuleFactory.java
+++ littleware/src/littleware/bootstrap/AppModuleFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Reuben Pasquini All rights reserved.
+ * Copyright 2011 http://code.google.com/p/littleware
  *
  * The contents of this file are subject to the terms of the
  * Lesser GNU General Public License (LGPL) Version 2.1.

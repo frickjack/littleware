ddf26ff552d31575fe29ab6ba2dd68b68bbe2baf
Reuben Pasquini reuben <Reuben Pasquini reuben@frickjack.com>
2013-07-12 17:10:52 -0500

    littleAsset: move AbstractAssetTest constants to Config to avoid junit dependency in client config, remove ThreadLocal dependency from JpaTransactionFactory

--- littleAsset/src/main/java/littleware/asset/client/test/AbstractAssetTest.java
+++ littleAsset/src/main/java/littleware/asset/client/test/AbstractAssetTest.java
@@ -27,25 +27,21 @@ import littleware.test.LittleTest;
  * with a suite:  suite.addTest ( provider.get().putName( "testWhatever" ) )
  */
 public abstract class AbstractAssetTest extends LittleTest {
-    private static final UUID testUserId = UUIDFactory.parseUUID( "7AC5D21049254265B224B7512EFCF0D1");
     public static UUID  getTestUserId() {
-        return testUserId;
+        return Config.getTestUserId();
     }
     
-    private static final String testUserName = "littleware.test_user";
     public static String getTestUserName() {
-        return testUserName;
+        return Config.getTestUserName();
     }
-    
-    private static final UUID testHomeId = UUIDFactory.parseUUID("D589EABED8EA43C1890DBF3CF1F9689A");
     
     public static UUID   getTestHomeId() {
-        return testHomeId;
+        return Config.getTestHomeId();
     }
     
     /** Lots of tests want to create test assets under littleware.test_home */
     public static String getTestHome() {
-        return "littleware.test_home";
+        return Config.getTestHome();
     }
 
     /**

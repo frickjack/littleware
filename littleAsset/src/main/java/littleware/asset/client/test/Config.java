b00349637d2831cc652d0511260babc5503e2666
Reuben Pasquini reuben <Reuben Pasquini reuben@frickjack.com>
2013-07-31 17:51:30 -0500

    littleAsset: move some asset.client.test id-constants out to a Config class to avoid junit entanglement with runtime initialization

---
+++ littleAsset/src/main/java/littleware/asset/client/test/Config.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2013 http://code.google.com/p/littleware
+ * 
+ * The contents of this file are available subject to the terms of the
+ * Lesser GNU General Public License (LGPL) Version 2.1.
+ * http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package littleware.asset.client.test;
+
+import java.util.UUID;
+import littleware.base.UUIDFactory;
+
+/**
+ * Just a container for some constants.
+ * Moved out from AbstractAssetTest to avoid runtime dependency on junit -
+ * db-init code accesses these constants.
+ */
+public final class Config {
+    private Config(){}
+    
+    private static final UUID testUserId = UUIDFactory.parseUUID( "7AC5D21049254265B224B7512EFCF0D1");
+    public static UUID  getTestUserId() {
+        return testUserId;
+    }
+    
+    private static final String testUserName = "littleware.test_user";
+    public static String getTestUserName() {
+        return testUserName;
+    }
+    
+    
+    private static final UUID testHomeId = UUIDFactory.parseUUID("D589EABED8EA43C1890DBF3CF1F9689A");
+    public static UUID   getTestHomeId() {
+        return testHomeId;
+    }
+    
+    /** Lots of tests want to create test assets under littleware.test_home */
+    public static String getTestHome() {
+        return "littleware.test_home";
+    }
+    
+}

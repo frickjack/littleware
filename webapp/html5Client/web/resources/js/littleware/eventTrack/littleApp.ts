0d4b800d5e1f5850cedd57d20b1883549b4b155b
Reuben Pasquini reuben <Reuben Pasquini reuben@frickjack.com>
2013-11-07 22:34:18 -0600

    html5Client: small navigation and UX tweaks

--- webapp/html5Client/web/resources/js/littleware/eventTrack/littleApp.ts
+++ webapp/html5Client/web/resources/js/littleware/eventTrack/littleApp.ts
@@ -453,9 +453,11 @@ export module littleware.eventTrack.littleApp {
                         // TODO - load template from somewhere
                         var index = mgr.routeIndex;
                         index.sort();
-                        var content = "<ul class='little-menu pure-menu pure-menu-open'>\n";
+                        var content = "<ul class='little-menu pure-menu pure-menu-open'>\n",
+                            label = "";
                         for (var i = 0; i < index.length; ++i) {
-                            content += "<li><a class='little-route' href='" + index[i] + "'>" + index[i] + "</a></li>";
+                            var label = index[i].replace(/^\/+/, "");
+                            content += "<li><a class='little-route' href='" + index[i] + "'>" + label + "</a></li>";
                         }
                         content += "</ul>";
                         log.log("Rendering index view: " + content);

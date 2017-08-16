6a815714caea3243c97a350b6a5390fd4561c125
Reuben Pasquini <reuben@frickjack.com>
2017-08-16 17:06:07 -0500

    ../s3Copy/ setup Dockerfile.s3cp
    Published public Docker image to hub.docker.com

--- webapp/littleApps/fishRunner/cli/fishRunner.sh
+++ webapp/littleApps/fishRunner/cli/fishRunner.sh
@@ -7,14 +7,10 @@ if [ ! -z ${JAVA_HOME} ]; then
     JAVACMD="${JAVA_HOME}/bin/java"
 fi
 
-DEFAULT_OPTS="-Xmx1024m -Xms128m -XX:+UseG1GC -Dnetworkaddress.cache.ttl=60"
-
-folder=$(dirname $0)
-cd "$folder"
-
 
 #rem %JAVACMD% -Xmx1024m -Xms1024m "-Djava.util.logging.config.file=%~dps0..\..\..\properties\logging.properties"  -cp "%SCALA_HOME%\lib\*;%~dps0..\ivy\test\*;%~dps0\..\build\classes" scala.tools.nsc.MainGenericRunner -usejavacp %*
+DEFAULT_OPTS="-Xmx1024m -Xms1024m -XX:+UseG1GC -Dnetworkaddress.cache.ttl=60"
 
-exec ${JAVACMD} ${DEFAULT_OPTS} -cp "../build/lib/*:../build/classes/main:../build/resources/main" littleware.apps.s3Copy.view.cli.CliCopyApp $@
+${JAVACMD} ${DEFAULT_OPTS} -cp "build/lib/*:build/classes/main:build/resources/main" littleware.apps.s3Copy.view.cli.CliCopyApp $@
 
 

d46de58ac10063556c6c4c362d9e6c350526da50
Reuben Pasquini <reuben@frickjack.com>
2021-03-05 07:10:52 -0600

    chore(littleScala): refine PropertyBuilder

--- littleScala/src/test/scala/littleware/scala/PropertyBuilderTester.scala
+++ littleScala/src/test/scala/littleware/scala/PropertyBuilderTester.scala
@@ -4,6 +4,8 @@ package littleware.scala
 import org.junit.Assert._
 import org.junit.Test
 
+import PropertyBuilder.{ rxValidator, notNullValidator, positiveIntValidator }
+
 class PropertyBuilderTester extends test.LittleTest {
   
   @Test
@@ -13,16 +15,21 @@ class PropertyBuilderTester extends test.LittleTest {
       val sanity = builder.checkSanity()
       assertTrue( "test builder starts in invalid state: " + sanity.mkString( ", " ), sanity.nonEmpty )
     }
-    builder.aString( "bla" ).iNumber( 5 ).bBuffer
-    import PropertyBuilder._
-    assertTrue( "null check does what it does", sanityCheck( nullCheck )(null).nonEmpty )
+    builder.aString( "bla" ).iNumber( 5 ).bBuffer.add("whatever").rxTest("123")
+    assertTrue("rxTest fails on bad data", !builder.rxTest.checkSanity().isEmpty)
+    builder.rxTest("abc123")
+    assertTrue("rxTest passes on good data", builder.rxTest.checkSanity().isEmpty)
   } catch basicHandler
 }
 
 object PropertyBuilderTester {
-  class TestBuilder extends PropertyBuilder {
-    val aString = new NotNullProperty[String]().name( "aString" )
-    val iNumber = new IntProperty().name( "iNumber" )
-    val bBuffer = new BufferProperty[String]().name( "bBuffer" )
+  class TestBuilder extends PropertyBuilder[String] {
+    val aString = new Property[String](null) withName "aString" withValidator notNullValidator
+    val iNumber = new Property(0) withName "iNumber" withValidator positiveIntValidator
+    val bBuffer = new BufferProperty[String]() withName "bBuffer"
+    val rxTest = new Property("") withName "rxTest" withValidator rxValidator(raw"[a-z][0-9a-z]+".r)
+
+    override def copy(v:String) = this
+    override def build():String = "whatever"
   }
 }
